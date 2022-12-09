9d986b4259dad6065077e876c9c0403a26f184b5
wmaldonado <wilysanchez10@gmail.com>
2022-12-09 14:17:34 -0500

    version 8 nuevas interacciones con el menu cambio botones

--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/MenuPage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/MenuPage.java
@@ -8,7 +8,7 @@ public class MenuPage extends PageObject {
 
     public static final Target ORGANIZATION_MENU = Target.the("item organization in menu").located(By.xpath("//span[contains(text(), 'Organization')]"));
     public static final Target UNITS_LINK = Target.the("item unit link").located(By.xpath("//span[contains(text(), 'Business Units')]"));
-    public static final Target NEW_UNIT_BUTTON= Target.the("new unit button").located(By.cssSelector("div.tool-button.add-button.icon-tool-button"));
+    public static final Target NEW_UNIT_BUTTON= Target.the("new unit button").located(By.xpath("//span[contains(text(), '  Nuevo Business Unit')]"));
     public static final Target NAME_INPUT = Target.the("input name").located(By.name("Name"));
     public static final Target PARENT_SELECT = Target.the("parent select").located(By.xpath("//span[@class='select2-arrow']"));
     public static final Target PARENT_TECH = Target.the("parent tech").located(By.xpath("//div[@id='select2-drop']//ul//li[6]"));

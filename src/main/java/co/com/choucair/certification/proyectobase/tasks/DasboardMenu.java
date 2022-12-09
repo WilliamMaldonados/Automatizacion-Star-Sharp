2942b77b4974aa5d640ed5e89e6d1cecf21fdaee
wmaldonado <wilysanchez10@gmail.com>
2022-12-09 10:28:01 -0500

    version 6 actulizacion botones

--- src/main/java/co/com/choucair/certification/proyectobase/tasks/DasboardMenu.java
+++ src/main/java/co/com/choucair/certification/proyectobase/tasks/DasboardMenu.java
@@ -5,9 +5,12 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.conditions.Check;
+import net.serenitybdd.screenplay.questions.CurrentVisibility;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static co.com.choucair.certification.proyectobase.userinterface.MeetingPage.*;
 import static co.com.choucair.certification.proyectobase.userinterface.MenuPage.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isCurrentlyVisible;
 
 
 public class DasboardMenu implements Task {
@@ -22,13 +25,11 @@ public class DasboardMenu implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Check.whether(objetive.equals("Meeting"))
+        actor.attemptsTo(
+                Check.whether(objetive.equals("Meeting"))
                         .andIfSo(Click.on(MEETINGS_LINK),
                                Click.on(MEETINGS_PAGE))
                                .otherwise(Click.on(ORGANIZATION_MENU),
                         Click.on(UNITS_LINK)));
-
-    }
-
-
-}
+        }
+     }
--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/MenuPage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/MenuPage.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.By;
 public class MenuPage extends PageObject {
 
    public static final Target ORGANIZATION_MENU = Target.the("item organization in menu").located(By.xpath("//span[contains(text(), 'Organization')]"));
-    public static final Target UNITS_LINK = Target.the("item organization in menu").located(By.xpath("//span[contains(text(), 'Business Units')]"));
-    public static final Target NEW_UNIT_BUTTON= Target.the("item organization in menu").located(By.xpath("//span[contains(text(), ' New Business Unit')]"));
-    public static final Target NAME_INPUT = Target.the("item organization in menu").located(By.name("Name"));
-    public static final Target PARENT_SELECT = Target.the("item organization in menu").located(By.xpath("//span[@class='select2-arrow']"));
-    public static final Target PARENT_TECH = Target.the("item organization in menu").located(By.xpath("//div[@id='select2-drop']//ul//li[6]"));
-    public static final Target CREATE_BUTTON = Target.the("item organization in menu").located(By.xpath("//span[contains(text(), ' Save')]"));
-    public static final Target Table_UNIT_CREATED = Target.the("item organization in menu").located(By.xpath("//a[contains(text(), 'UnidadWill')]"));
+    public static final Target UNITS_LINK = Target.the("item unit link").located(By.xpath("//span[contains(text(), 'Business Units')]"));
+    public static final Target NEW_UNIT_BUTTON= Target.the("new unit button").located(By.xpath("//span[contains(text(), ' New Business Unit')]"));
+    public static final Target NAME_INPUT = Target.the("input name").located(By.name("Name"));
+    public static final Target PARENT_SELECT = Target.the("parent select").located(By.xpath("//span[@class='select2-arrow']"));
+    public static final Target PARENT_TECH = Target.the("parent tech").located(By.xpath("//div[@id='select2-drop']//ul//li[6]"));
+    public static final Target CREATE_BUTTON = Target.the("button create").located(By.xpath("//span[contains(text(), ' Save')]"));
+    public static final Target Table_UNIT_CREATED = Target.the("table unit create").located(By.xpath("//a[contains(text(), 'UnidadWill')]"));
 }

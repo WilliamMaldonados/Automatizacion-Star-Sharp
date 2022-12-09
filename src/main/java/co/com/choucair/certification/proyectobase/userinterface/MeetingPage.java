bf344fba74233d0825cd06e12407e46b9a8ef5d6
wmaldonado <wilysanchez10@gmail.com>
2022-12-09 09:15:39 -0500

    version 5 actulizacion booton meeting

--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/MeetingPage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/MeetingPage.java
@@ -8,7 +8,7 @@ public class MeetingPage extends PageObject {
 
     public static final Target MEETINGS_LINK = Target.the("link of the meetings").located(By.xpath("//span[contains(text(), 'Meeting')]"));
     public static final Target MEETINGS_PAGE = Target.the("link of the meetings").located(By.xpath("//span[contains(text(), 'Meetings')]"));
-    public static final Target NEW_MEETING_BUTTON = Target.the("button to create a new meeting").located(By.xpath("//span[contains(text(), ' New Meeting')]"));
+    public static final Target NEW_MEETING_BUTTON = Target.the("button to create a new meeting").located(By.cssSelector("div.tool-button.add-button.icon-tool-button"));
     public static final Target NAME_INPUT = Target.the("where do we write the name of the meeting").located(By.name("MeetingName"));
     public static final Target TYPE_SELECT = Target.the("select the type of meeting").located(By.xpath("//div[@class='select2-container select2-allowclear editor s-Serenity-LookupEditor s-LookupEditor has-inplace-button required']"));
     public static final Target TYPE_WEEKLY = Target.the("select the type of meeting").located(By.xpath("//div[@id='select2-drop']//ul//li[6]"));
